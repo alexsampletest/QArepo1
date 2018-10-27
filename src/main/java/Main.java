d97587086df28828dac6eacfd3314d2e6ce1a4ad
alexsampletest <alexsamplegitspace@gmail.com>
2018-10-27 18:17:09 -0400

    added apple image background

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,11 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -25,7 +29,35 @@ public class Main extends Application {
         });
 
         StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        VBox vbox = new VBox();
+
+        vbox.getChildren().add(btn);
+        Button banana = new Button(("Banana"));
+        vbox.getChildren().add(banana);
+        banana.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("banana !!!");
+            }
+        });
+
+        Button potato = new Button(("Potato"));
+        vbox.getChildren().add(potato);
+        potato.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("potato !!!");
+            }
+        });
+
+        Image image1 = new Image(Main.class.getResourceAsStream("apple.jpg"));
+        ImageView imageView = new ImageView(image1);
+        GridPane gridpane = new GridPane();
+        gridpane.getChildren().add(imageView);
+        gridpane.getChildren().add(vbox);
+        GridPane.setConstraints(imageView, 0, 4);
+
+        root.getChildren().add(gridpane);
         primaryStage.setScene(new Scene(root, 300, 250));
         primaryStage.show();
     }
