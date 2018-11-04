e7fd628c59ca67d373541563a600aae4bcd232a5
alexsampletest <alexsamplegitspace@gmail.com>
2018-11-04 13:25:38 -0500

    added extra tests

---
+++ src/test/java/StandardTests.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+//https://junit.org/junit5/docs/current/user-guide/
+public class StandardTests {
+    @Test
+    void succeedingTest() {
+        assertNotEquals(2, 1 + 1);
+    }
+
+
+    @Test
+    void failingTest() {
+        assertTrue("Maxim".startsWith("M"));
+        //assertEquals(true, "Maxim".startsWith("M"));
+    }
+
+    @Test
+    void failingTest2() {
+        assertTrue("Maxim".startsWith("zz"));
+    }
+
+}
